b094ccf27fb77ef55c5e5d3bf4c6cbe97917df8c
Phil smith <pcsmith86@gmail.com>
2017-09-10 09:50:03 +0100

    Set up routing with spec

---
+++ spec/routing/shorteners_routing_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe ShortenersController, type: :routing do
+  describe "routing" do
+
+    it "routes to #new" do
+      expect(:get => "/").to route_to("shorteners#new")
+    end
+
+    it "routes to #redirect" do
+      expect(:get => "/abc123").to route_to("shorteners#redirect", :path => "abc123")
+    end
+
+    it "routes to #create" do
+      expect(:post => "/").to route_to("shorteners#create")
+    end
+  end
+end
